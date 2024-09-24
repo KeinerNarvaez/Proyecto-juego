da64fd9ea03ec9f9fd4750c2ea983875b47386ca
Francisco <franciscoandradebermeo560@gmail.com>
2024-09-23 20:16:16 -0500

    diseño del login listo

--- js/clickleo.js
+++ js/clickleo.js
@@ -1,8 +1,23 @@
+// Selecciona todos los botones con la clase "text-boton"
 const buttons = document.querySelectorAll(".text-boton");
+// Selecciona el elemento de audio
 const audio = document.getElementById("clickSound");
 
+// Añade el evento "click" a cada botón
 buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        audio.play();
+    button.addEventListener("click", (event) => {
+        audio.play(); // Reproduce el sonido
+        
+        // Si el botón tiene un enlace (como en el caso de JUGAR)
+        if (button.getAttribute("href")) {
+            event.preventDefault(); // Evita la redirección inmediata
+            setTimeout(() => {
+                window.location.href = button.getAttribute("href"); // Redirige después de un pequeño retraso
+            }, 300); // 300ms de retraso para que el sonido se reproduzca
+        }
     });
 });
+
+
+const inputs = document.querySelectorAll("inputs");
+
