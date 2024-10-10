d9edab0ddaaf9f5fdbe92248889bbbdcd782e4c9
Francisco <franciscoandradebermeo560@gmail.com>
2024-10-09 23:09:19 -0500

    modales ya implementados

--- js/codigo_veri_cuenta.js
+++ js/codigo_veri_cuenta.js
@@ -7,7 +7,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
         // Unificar los inputs del código que brinda el usuario
         const codigo = [
-<<<<<<< HEAD
             document.getElementById('input1-codigo').value,
             document.getElementById('input2-codigo').value,
             document.getElementById('input3-codigo').value,
@@ -15,15 +14,6 @@ window.addEventListener('DOMContentLoaded', () => {
             document.getElementById('input5-codigo').value,
             document.getElementById('input6-codigo').value
         ].join(''); // Unir los 6 inputs
-=======
-            document.getElementById('input1').value,
-            document.getElementById('input2').value,
-            document.getElementById('input3').value,
-            document.getElementById('input4').value,
-            document.getElementById('input5').value,
-            document.getElementById('input6').value
-        ].join(''); //las unifico los 6
->>>>>>> 168af8e860bbf2dd50049d750e278b9a0f516de2
 
         // Empaquetar el código en un objeto
         const data = {
@@ -41,12 +31,35 @@ window.addEventListener('DOMContentLoaded', () => {
         .then(response => response.json())
         .then(result => {
             if (result.status === 'success') {
-                alert('Cuenta verificada correctamente, ya puedes iniciar sesion normalmente.');
+                // Si el código es correcto, mostrar un mensaje de éxito
+                const mensajeModal = new bootstrap.Modal(document.getElementById('mensajeModal'));
+                const mensajeModalBody = document.getElementById('mensajeModalBody');
+                
+                mensajeModalBody.innerHTML = `
+                <div class="alert alert-secondary" style="text-align: center; margin-top:-12px;" >
+                    Cuenta verificada correctamente, ya puedes iniciar sesión normalmente. 
+                    <br> 
+                    <i class="fa-solid fa-check" style="display: block; font-size: 80px; margin: 20px auto;"></i>
+                </div>
+             `;
+
+                mensajeModal.show();
+
                 setTimeout(() => {
                     window.location.href = 'login.html'; // Redirigir después de 3 segundos
                 }, 3000);
             } else {
-                alert(result.message); // Mostrar mensaje de error
+                      // Mostrar modal de error 
+                      const mensajeModal = new bootstrap.Modal(document.getElementById('mensajeModal'));
+                      const mensajeModalBody = document.getElementById('mensajeModalBody');
+                      mensajeModalBody.innerHTML = `
+                        <div class="alert alert-danger" style="font-size: 70px;">
+                            ${result.message}
+                            <br>
+                            <i class="fa-solid fa-xmark" style="display: flex; justify-content: center; font-size: 120px; color: red; margin-left:350px;"></i>
+                        </div>
+                    `;
+                    mensajeModal.show();
             }
         })
         .catch(error => {
--- js/renovar_contrasena.js
+++ js/renovar_contrasena.js
@@ -8,7 +8,17 @@ window.addEventListener('DOMContentLoaded', () => {
         const contrasenaVerificada = document.getElementById('contrasena-verificada').value;
 
         if (contrasenaNueva !== contrasenaVerificada) {
-            alert("Las contraseñas no son iguales");
+            // Mostrar modal de error 
+            const mensajeModal = new bootstrap.Modal(document.getElementById('mensajeModal'));
+            const mensajeModalBody = document.getElementById('mensajeModalBody');
+            mensajeModalBody.innerHTML = `
+                <div class="alert alert-danger" style="font-size: 70px;">
+                    Las contraseñas no son iguales
+                    <br>
+                    <i class="fa-solid fa-xmark" style="display: flex; justify-content: center; font-size: 120px; color: red; margin: 0 auto;"></i>
+                </div>
+            `;
+            mensajeModal.show();
             return;
         }
 
@@ -25,12 +35,35 @@ window.addEventListener('DOMContentLoaded', () => {
         .then(response => response.json())
         .then(result => {
             if (result.status === 'success') {
-                alert('Contraseña renovada correctamente');
+                // Si la renovación de contraseña fue exitosa, mostrar un mensaje de éxito
+                const mensajeModal = new bootstrap.Modal(document.getElementById('mensajeModal'));
+                const mensajeModalBody = document.getElementById('mensajeModalBody');
+                
+                mensajeModalBody.innerHTML = `
+               <div class="alert alert-secondary" style="text-align: center; height: 350px;">
+                   <h1 style="font-size: 65px;"> Contraseña renovada correctamente  </h1>
+                   <br> 
+                   <i class="fa-solid fa-check" style="display: block; font-size: 80px; margin: 0 auto; margin-top: -12px;"></i>
+              </div>
+             `;
+
+                mensajeModal.show();
+
                 setTimeout(() => {
                     window.location.href = 'index.html';
-                }, 5000);
+                }, 3000);
             } else {
-                alert(result.message);
+              // Mostrar modal de error 
+              const mensajeModal = new bootstrap.Modal(document.getElementById('mensajeModal'));
+              const mensajeModalBody = document.getElementById('mensajeModalBody');
+              mensajeModalBody.innerHTML = `
+                <div class="alert alert-danger" style="font-size: 70px;">
+                    ${result.message}
+                    <br>
+                    <i class="fa-solid fa-xmark" style="display: flex; justify-content: center; font-size: 120px; color: red; margin: 0 auto;"></i>
+                </div>
+            `;
+            mensajeModal.show();
             }
         })
         .catch(error => {
